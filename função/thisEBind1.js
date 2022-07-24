3903a2296198bf4cb5f2811dccbe1d0cf931bce5
João Gabriel <joao.nitsus@gmail.com>
2022-07-24 15:36:26 -0300

    att

--- função/thisEBind1.js
+++ função/thisEBind1.js
@@ -1,11 +1,16 @@
 const pessoa = {
-    saudacao: 'Bom dia!',
-    falar() {
-        console.log(this.saudacao)
+    saudacao: 'Bom dia',
+    falar () {
+      console.log(this.saudacao)
     }
-}
-
-pessoa.falar()
-const falar = pessoa.falar
-falar() // conflito entre paradigmas OO e 
-
+  }
+  
+  pessoa.falar()
+  const falar = pessoa.falar
+  falar()  // conflito entre paradigmas: funcional e OO
+  
+  // bind você passa um objeto para ser resolvido 
+  
+  const falarDePessoa = pessoa.falar.bind(pessoa)
+  falarDePessoa()
+  
