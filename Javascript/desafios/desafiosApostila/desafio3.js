e813dfa883a2ff4dbab608c6414dc2670b81f17f
João Gabriel <joao.nitsus@gmail.com>
2022-09-04 15:58:33 -0300

    att

---
+++ Javascript/desafios/desafiosApostila/desafio3.js
@@ -0,0 +1,8 @@
+// Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente
+
+function baseElevada (base, expoente) {
+    resultado = base ** expoente
+    return resultado
+}
+
+console.log(baseElevada(2, 3))
