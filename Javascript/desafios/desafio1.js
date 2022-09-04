5542d637541c8d54cf5cdb097130aa00b528a20b
João Gabriel <joao.nitsus@gmail.com>
2022-09-04 14:05:09 -0300

    att

---
+++ Javascript/desafios/desafio1.js
@@ -0,0 +1,6 @@
+
+function Calcularoperações (operador1, operador2) {
+    console.log(operador1 - operador2, operador1 + operador2, operador1 * operador2, operador1 / operador2)
+} 
+
+console.log(Calcularoperações(2, 3))
