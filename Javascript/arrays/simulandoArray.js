e8dfaf60545adc0b7f43ef1587ecfdfe86e7569b
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-10-25 15:13:26 -0300

    att

--- Javascript/arrays/simulandoArray.js
+++ Javascript/arrays/simulandoArray.js
@@ -1,1 +1,12 @@
-const
+const quaseArray = { 0: 'Rafael', 1: 'Sthefanie', 2: 'João' }
+console.log(quaseArray)
+Object.defineProperty(quaseArray, 'toString', {
+    value: function() { return Object.values(this) },
+    enumerable: false
+})
+
+console.log(quaseArray[0])
+
+const meuArray = ['Rafael', 'Sthefanie', 'João']
+console.log(quaseArray.toString(), meuArray)
+
