ad3744ae70d13ef8725c81d5c023781b3e9e70e9
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2023-01-09 17:10:50 -0300

    att

--- Javascript/arrays/imperativoVSDeclarativo.js
+++ Javascript/arrays/imperativoVSDeclarativo.js
@@ -1,3 +1,21 @@
 const alunos = [
-    { nome: 'João', nota: 10     }
+    { nome: 'João', nota: 7.9 },
+    { nome: 'Sthefanie', nota: 9.2 }
 ]
+
+// Imperativo: ensinar os passos exatamente como funciona (o foco é em como deve ser feito)
+let total1 = 0
+for(let i = 0; i < alunos.length; i++) {
+    total1 += alunos[i].nota
+}
+console.log(total1 / alunos.length)
+
+// Declarativo: o foco é o que deve ser feito
+const getNota = aluno => aluno.nota
+const soma = (total, atual) => total + atual
+const total2 = alunos.map(getNota).reduce(soma) // Reduce pode ser usado como agregador
+console.log(total2 / alunos.length)
+
+
+// Diferença: a abordagem Declarativa é mais interessante
+//            por conta de reuso das funções em vários locais diferentes
